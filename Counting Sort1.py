725f09993ff9f9e8f6c8a1d70ba03a58b2bf3235
Tewobsta Seyoum Assefa <155534908+Tewobsta-sa@users.noreply.github.com>
2024-04-01 14:51:11 +0300

    Create Counting Sort1.py

---
+++ Counting Sort1.py
@@ -0,0 +1,20 @@
+def countingSort(arr):
+    # Write your code here
+    count = [0] * 100
+    for num in arr:
+        count[num] += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
