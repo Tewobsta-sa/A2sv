5ce8e4c67abb16d5490a71b3ecb242192a10115d
TewobstaSeyoum <155534908+Tewobsta-sa@users.noreply.github.com>
2024-08-14 00:24:31 +0300

    Update Number-of-Smaller.py

--- Number-of-Smaller.py
+++ Number-of-Smaller.py
@@ -4,7 +4,7 @@ list2 = list(map(int, input().split()))
 x = 0
 cou = []
 for i in range(m):
-    while x < n and list1[x] <= list2[i]:
+    while x < n and list1[x] < list2[i]:
         x += 1
     cou.append(x)
 print(' '.join(map(str, cou)))
